4c70c9b0ce09930e5394cf010d6a665e32a00bdd
SofiaLampropoulou <167758102+SofiaLampropoulou@users.noreply.github.com>
2025-05-02 16:23:54 +0300

    Create ContactController.php

---
+++ modules/contact/controllers/ContactController.php
@@ -0,0 +1,63 @@
+<?php
+
+class ContactController extends Controller{
+
+    function runBeforeAction(){
+       // var_dump($_SESSION);
+            if ($_SESSION['has_submitted_the_form'] ?? 0 == 1){
+             
+        $dbh = DatabaseConnection::getInstance();
+        $dbc = $dbh->getConnection();
+
+
+        $pageObject = new Page($dbc);
+        $pageObject->findBy('id',$this->entityId);
+        $variables['pageObject'] = $pageObject;
+
+              
+                $template = new Template('default');
+                $template -> view('page/views/staticPage', $variables);
+                //include 'view/contact/contact_us_already_contacted.html';
+                return false;
+            }
+          return true;
+        }
+
+    function defaultAction(){
+        $variables['title']='';
+        $variables['content']='';
+        $dbh = DatabaseConnection::getInstance();
+        $dbc = $dbh->getConnection();
+    
+        $pageObject = new Page($dbc);
+        $pageObject->findById(5);
+        $variables['pageObject'] = $pageObject;
+
+       
+      
+        $template = new Template('default');
+        $template -> view('contact/views/contact/contact_us', $variables);
+}
+    function submitContactFormAction(){
+     
+        //validate
+    //store data
+    //send email
+    $_SESSION['has_submitted_the_form'] = 1;
+    $dbh = DatabaseConnection::getInstance();
+    $dbc = $dbh->getConnection();
+
+
+    $pageObject = new Page($dbc);
+    $pageObject->findById(4);
+    $variables['pageObject'] = $pageObject;
+  
+    $template = new Template('default');
+    $template -> view('page/views/staticPage', $variables);
+   // include 'view/contact/contact_us_thank_you.html';
+}
+
+
+} 
+    
+
